f18461c43f63513884de5df131ace0f546dc8b55
Charles Heisler <charles.heisler@hds.com>
2017-11-20 15:51:02 -0500

    improved definition and parsing of phonetics

--- phonetics.py
+++ phonetics.py
@@ -13,14 +13,37 @@ class PhoneticsEngine(object):
     def __init__(self, filename):
         with open(filename, 'rb') as file:
             config = yaml.load(file)
+        self._load_segments(config)
+
+    def _load_segments(self, config):
+        """ Load segments from configuration and group  by features. """
+
+        stack = [([], config['segments'])]
         self._segments = set()
-        self._classes = dict(set)
-        stack = []
-        for segment, features in config['segments'].items():
-            self._segments.add(segment)
-            for feature in features:
-                self._classes[feature].add(segment)
+        self._classes = defaultdict(set)
+
+        # find all segments and add them to any ancestor feature classes
+        while len(stack) > 0:
+            features, segments = stack.pop()
+
+            # if value is list add segments to parent classes
+            if isinstance(segments, list):
+                for feature in features:
+                    for segment in segments:
+                        self._segments.add(segment)
+                        self._classes[feature].add(segment)
+
+            # if value is a dictionary add another stack item for each entry
+            elif isinstance(segments, dict):
+                for key, value in segments.items():
+                    stack.append((features + [key], value))
+
+            # otherwise the configuration is badly formed
+            else:
+                raise Exception("NO")
+
         self._classes = dict(self._classes)
+
 
     TARGET = '_'
     BOUNDARY = '#'
@@ -57,17 +80,41 @@ class PhoneticsEngine(object):
         return changes[match.group(0)]
 
 
-    # tokens for writing operations
+    # definitions of functions for phonetic class expressions
+    class Operator(object):
+        def __init__(self, token, regex, arity, precedence, function):
+            self.token = token
+            self.regex = regex
+            self.arity = arity
+            self.precedence = precedence
+            self.function = function
+
+    NOT = Operator(token='-', regex=r'-(?=\[)', arity=1, precedence=2, function='_not')
+    AND = Operator(token=',', regex=r',', arity=2, precedence=1, function='_and')
+    OR = Operator(token='|', regex=r'\|', arity=2, precedence=0, function='_or')
+    OPERATORS = {op.token: op for op in (NOT, AND, OR)}
+
+    def _not(self, x):
+        return self._segments - x
+
+    def _and(self, x, y):
+        return x & y
+
+    def _or(self, x, y):
+        return x | y
+
+
+    # regex definitions for matching phonetic expression tokens
     CLASS = r'[+-][\w-]+'
-    AND = r','
-    OR = r'|'
-    LEFT_BRACKET = r'['
-    RIGHT_BRACKET = r']'
-    OPERATOR = _set_regex(AND, re.escape(OR))
-    TOKEN = _set_regex(CLASS, AND, re.escape(OR), re.escape(LEFT_BRACKET), re.escape(RIGHT_BRACKET), '.')
-    PRECEDENCE = {AND: 1, OR: 0, LEFT_BRACKET: -1}
-    FUNCTIONS = {AND: operator.__and__, OR: operator.__or__}
+    LEFT_BRACKET = r'\['
+    RIGHT_BRACKET = r'\]'
+    OPERATOR = _set_regex(*[op.regex for op in OPERATORS.values()])
+    TOKEN = _set_regex(CLASS, OPERATOR, LEFT_BRACKET, RIGHT_BRACKET, '.')
+
+
     def parse(self, expr):
+        """ Parse an expression representing a set of segments. """
+
         out = []
         ops = []
 
@@ -80,20 +127,23 @@ class PhoneticsEngine(object):
                 out.append(token)
 
             # if operator apply previous operators if necessary and add to stack
-            elif re.match(self.OPERATOR, token):
-                while len(ops) > 0 and self.PRECEDENCE[ops[-1]] >= self.PRECEDENCE[token]:
-                    out.append(ops.pop())
-                ops.append(token)
+            elif token in self.OPERATORS:
+                op = self.OPERATORS[token]
+                while len(ops) > 0 and isinstance(ops[-1], self.Operator) \
+                and ops[-1].precedence >= op.precedence:
+                    out.append(ops.pop().token)
+                ops.append(op)
 
             # if left bracket add to operator stack
-            elif token == self.LEFT_BRACKET:
+            elif re.match(self.LEFT_BRACKET, token):
                 ops.append(token)
 
             # if right bracket apply operators until left bracket 
-            elif token == self.RIGHT_BRACKET:
-                while len(ops) > 0 and ops[-1] != self.LEFT_BRACKET:
-                    out.append(ops.pop())
-                if len(ops) == 0:
+            elif re.match(self.RIGHT_BRACKET, token):
+                while len(ops) > 0 and isinstance(ops[-1], self.Operator):
+                    out.append(ops.pop().token)
+                if len(ops) == 0 or not re.match(self.LEFT_BRACKET, ops[-1]):
+                    print ops
                     raise ExpressionException("no matching left bracket")
                 ops.pop()
 
@@ -103,19 +153,22 @@ class PhoneticsEngine(object):
         
         # apply any remaining operators
         while len(ops) > 0:
-            out.append(ops.pop())
+            out.append(ops.pop().token)
 
         # return the parsed output
         return out
 
 
     def eval(self, tokens):
+        """ Evaluate a set of parsed tokens into a set of segments. """
+
         out = []
         for token in tokens:
+
+            # if token is value put appropriate segment set on stack
             if re.match(self.CLASS, token):
                 try:
                     if token[0] == '+':
-                        print 'om'
                         out.append(self._classes[token[1:]])
                     elif token[0] == '-':
                         out.append(self._segments - self._classes[token[1:]])
@@ -124,28 +177,26 @@ class PhoneticsEngine(object):
                 except KeyError:
                     raise ExpressionException("invalid feature: '%s'" % token)
 
-            elif re.match(self.OPERATOR, token):
+            # if token is operator apply to top of stack
+            elif token in self.OPERATORS:
                 try:
-                    x = out.pop()
-                    y = out.pop()
-                    out.append(self.FUNCTIONS[token](x, y))
+                    op = self.OPERATORS[token]
+                    fn = getattr(self, op.function)
+                    args = []
+                    for i in range(op.arity):
+                        args.append(out.pop())
+                    out.append(fn(*args))
                 except IndexError:
                     raise Exception("NO")
 
+            # raise exception on invalid token
             else:
                 raise ExpressionException("invalid token: '%s'" % token)
+
+        # check for errors and return the final value on top of the stack
+        if len(out) > 1 or not isinstance(out[0], set):
+            raise Exception("NO")
         return out.pop()
-
-
-    @classmethod
-    def _op(cls, out, ops):
-        x = out.pop()
-        y = out.pop()
-        op = ops.pop()
-        try:
-            fn = FUNCTIONS[op](x, y)
-        except KeyError:
-            raise Exception("invalid")
 
 
 class ExpressionException(Exception):
